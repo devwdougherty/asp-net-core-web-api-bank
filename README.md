03bce540de19980a92ebdb243903140a2a410790
Willian Dougherty <willian.barbosa@fit-tecnologia.org.br>
2019-05-02 23:22:49 -0300

    initial - first model + db context (ef core)

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # asp-net-core-web-api-bank
+
+nuget
+
+entity framework 6.2.0
+
+package manager console:
+
+1 - Add-Migration "InitialCreate" or EntityFrameworkCore\Add-Migration "InitialCreate"
+2 - Update-Database or EntityFrameworkCore\Update-Database
---
+++ WebAPIBank/Models/PaymentDetailContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPIBank.Models
+{
+    public class PaymentDetailContext : DbContext
+    {
+
+        // Constructor for DI - Database String conection -> options
+        public PaymentDetailContext(DbContextOptions<PaymentDetailContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<PaymentDetail> PaymentDetails { get; set; }
+    }
+}
