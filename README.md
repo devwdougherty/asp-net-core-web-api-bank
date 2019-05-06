b15cfff69fc72d67874229d6c9cd7ff0f1958dc9
Willian Dougherty <willian.barbosa@fit-tecnologia.org.br>
2019-05-05 22:32:40 -0300

    update readme

--- README.md
+++ README.md
@@ -8,3 +8,19 @@ package manager console:
 
 1 - Add-Migration "InitialCreate" or EntityFrameworkCore\Add-Migration "InitialCreate"
 2 - Update-Database or EntityFrameworkCore\Update-Database
+
+json custom serializer to maintain the capitalletters on database fields instead camelcase -> setup.cs configureservices
+
+boostrap layout
+fontawsome icons
+
+PaymentDetails - created without default style
+
+ng g n component
+--spec=false or skipTests
+--s = no style
+
+
+
+Generate a model class
+ng cl (or class) payment-detail --type=model 
--- WebAPIBank/Startup.cs
+++ WebAPIBank/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Newtonsoft.Json.Serialization;
 using WebAPIBank.Models;
 
 namespace WebAPIBank
@@ -26,7 +27,16 @@ namespace WebAPIBank
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_1)
+                /* Willian: Use the default field names definied on the API instead CamelCase pattern */
+                .AddJsonOptions(options => {
+                    var resolver = options.SerializerSettings.ContractResolver;
+                    if (resolver != null)
+                    {
+                        (resolver as DefaultContractResolver).NamingStrategy = null;
+                    }
+                });
 
             services.AddDbContext<PaymentDetailContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("DevConnection")));
